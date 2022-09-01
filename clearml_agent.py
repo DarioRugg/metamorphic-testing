7d0245ce4dae7fb0085196baa08b95c7f46abd0c
ruggeri <ruggeri@inf.u-szeged.hu>
2022-09-01 14:21:35 +0200

    osing my docker image for agent

--- clearml_agent.py
+++ clearml_agent.py
@@ -21,13 +21,24 @@ def main(cfg : DictConfig) -> None:
         project_name='e-muse/DeepMS',
         task_name='Hyper-Parameter Optimization'
     )
+    # task.set_base_docker(
+    #     docker_image='clearml-python-v3x:latest',
+    #     docker_arguments='--env GIT_SSL_NO_VERIFY=true \
+    #                     --env CLEARML_AGENT_GIT_USER=glbot-deepms \
+    #                     --env CLEARML_AGENT_GIT_PASS=glpat-CYE1apje2yyBhTHtVXFj \
+    #                     --env LOCAL_PYTHON=python{}.{}'.format(sys.version_info.major,
+    #                                                             sys.version_info.minor),
+    # )
+
+    # with my container
     task.set_base_docker(
-        docker_image='clearml-python-v3x:latest',
+        docker_image='rugg/deepms:latest',
         docker_arguments='--env GIT_SSL_NO_VERIFY=true \
                         --env CLEARML_AGENT_GIT_USER=glbot-deepms \
                         --env CLEARML_AGENT_GIT_PASS=glpat-CYE1apje2yyBhTHtVXFj \
-                        --env LOCAL_PYTHON=python{}.{}'.format(sys.version_info.major,
-                                                                sys.version_info.minor),
+                        --env CLEARML_AGENT_SKIP_PIP_VENV_INSTALL=true' \
+                        # --env LOCAL_PYTHON=python{}.{}'.format(sys.version_info.major,
+                        #                                         sys.version_info.minor),
     )
 
     hyper_parameters=[
