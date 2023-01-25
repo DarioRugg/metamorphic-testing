5b7a6137ea57c48c675f4fcaeb0b2451b521904c
DarioRugg <dario.ruggeri.ai@gmail.com>
2023-01-25 11:22:09 +0100

    useless test

--- test_2.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.patches as mpatches
-import matplotlib.pyplot as plt
-
-fig, ax = plt.subplots()
-red_patch = mpatches.Patch(color='red', label='The red data')
-ax.legend(handles=[red_patch])
-
-plt.show()
